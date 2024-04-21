afa54ccbeef7fb120d4c235be41b164303c20c6e
l-burrell <eewinterz@gmail.com>
2024-04-21 22:36:27 +0000

    initial watcher.sh setup

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,23 @@
-#TODO
+#!/bin/bash
+
+namespace="sre"
+deployment="swype-app"
+maximum_restarts_allowed=3
+
+while true; do
+    # check that the pod restarts:
+    number_of_restarts=$(kubectl get pods --namespace "${namespace}" -l app="${deployment}" -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+
+    # display the number of restarts we have done:
+    echo "total number of restarts: $number_of_restarts"
+
+    # check restart limit:
+    if (( number_of_restarts > maximum_restarts_allowed )); then 
+        echo "No remaining restarts. Slowing down the deployment"
+        kubectl scale --replicas=0 deployment/"${deployment}"
+        break
+    fi
+
+    # pause the deployment for 60 seconds
+    sleep 60
+done
